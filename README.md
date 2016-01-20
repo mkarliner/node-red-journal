83352687dca1239315df0bcc40add5b1ffbe0439
Michael Karliner <mike@modern-industry.com>
2016-01-20 18:40:27 +0000

    NEW: outputs with timestamps and just plain

--- README.md
+++ README.md
@@ -2,14 +2,23 @@
 
 This node take a series of  messages assumed to be numeric values or objects, parses them
 and stores them in a fixed sized FIFO array. Each time a message
-arrives, it sends out the fifo.
+arrives, it sends out the fifo in two different versions on the first two outputs.
+
+The first version emits an array of objects which look like this:
+
+{
+	value: <inputValue>
+	journalTimestamp: <javascript timestamp in milliseconds>
+}
+
+The second version is a simple array of the last 'n' input values.
 
 Optionally, the journal can be persisted. If this is used, the storeName option
 should be set to a unique string and the interval should be set to the number
 of seconds between persisting the values.
 
 The node will also send an average of all the values in the fifo every _fifo-length_ messages 
-to the second output. This means the nodes can be cascaded to implement
+to the third output. This means the nodes can be cascaded to implement
 a round-robin style, in-core, time series database.
 
 This node has been designed to be used with ThingStudio and the widgets which
--- journal.js
+++ journal.js
@@ -6,31 +6,34 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
 		this.max = config.max;
 		this.storeName = config.storeName;
-		console.log("STORENAME: ", this.storeName)
+		//console.log("STORENAME: ", this.storeName)
 		this.persist = config.persist;
 		this.persistInterval  = config.persistInterval;
         var node = this;
 		storage.initSync({
 			interval: 1000 * this.persistInterval
 		});
-		fifo = storage.getItem(this.storeName);
-		console.log("FIFO INIT: ", fifo);
+		var fifo = storage.getItem(this.storeName);
+		//console.log("FIFO INIT: ", fifo);
         var fifo  = fifo || new Fifo(node.max, []);
 		
 		var cnt = 0;
         this.on('input', function(msg) {
 			// Push the value on to the fifo and output the fifo as a message.
-			var values = msg.payload;
-            fifo.push(values);
-	    	var newMsg = {payload: fifo }
-            node.send([newMsg, null]);
+			var value = msg.payload;
+            fifo.push({value: value, journalTimestamp: new Date().getTime()});
+	    	var objectOutput = {payload: fifo }
+			var simpleOutput = {payload: fifo.map(function(obj) {
+				return obj.value;
+			})};
+            node.send([objectOutput, simpleOutput, null]);
 			storage.setItem(this.storeName, fifo);
 			// Output the average every 'n' samples n the second output.
 			if(cnt == fifo.length-1) {
 				cnt=0;
 				var sum  = fifo.reduce(function(a, b) { return a + b; }, 0);
 				var average  = sum /  fifo.length;
-				node.send([null,{payload: average}])
+				node.send([null,null, {payload: average}])
 			} else {
 				cnt++;
 			}
